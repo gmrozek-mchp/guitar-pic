ffa0fd56a8f704387249f3a97128607f2f789f90
Greg Mrozek <greg.mrozek@microchip.com>
2026-03-26 11:14:00 -0500

    Create new VSCode MPLAB fretboard project.

---
+++ firmware/fretboard/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author greg
+ * @date 2026-03-26
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
