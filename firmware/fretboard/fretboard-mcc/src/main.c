f06bc5a094b1c2a7fcca54ec41919bc14644c0ec
Greg Mrozek <greg.mrozek@microchip.com>
2026-03-26 10:28:04 -0500

    Minimal test project of MCC generated code.

--- firmware/fretboard/fretboard-mcc/src/main.c
+++ firmware/fretboard/fretboard-mcc/src/main.c
@@ -24,6 +24,7 @@
 
 #include <stddef.h>                     // Defines NULL
 #include <stdbool.h>                    // Defines true
+#include <stdint.h>
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include "definitions.h"                // SYS function prototypes
 
@@ -34,15 +35,33 @@
 // *****************************************************************************
 // *****************************************************************************
 
+uint16_t adc_count;
+
 int main ( void )
 {
     /* Initialize all modules */
     SYS_Initialize ( NULL );
 
+    ADC0_Enable();
+    
     while ( true )
     {
         /* Maintain state machines of all polled MPLAB Harmony modules. */
         SYS_Tasks ( );
+
+        /* Start ADC conversion */
+        ADC0_ConversionStart();
+
+        /* Wait till ADC conversion result is available */
+        while(!ADC0_ResultReadyStatusGet())
+        {
+
+        };
+
+        /* Read the ADC result */
+        adc_count = ADC0_ConversionResultGet();
+        
+        SERCOM1_USART_Write( ".", 1 );
     }
 
     /* Execution should not come here during normal operation */
