3b276840d013113b52ff96a788ac6174f41c1727
Greg Mrozek <greg.mrozek@microchip.com>
2026-03-27 09:16:58 -0500

    Remove pressed data from datastream. Only raw data now.

--- firmware/fretboard/data_stream.c
+++ firmware/fretboard/data_stream.c
@@ -12,15 +12,10 @@ typedef struct __attribute__((packed)) {
     uint16_t yellow;
     uint16_t blue;
     uint16_t orange;
-    uint8_t  green_pressed;
-    uint8_t  red_pressed;
-    uint8_t  yellow_pressed;
-    uint8_t  blue_pressed;
-    uint8_t  orange_pressed;
     uint8_t  end;
 } ds_frame_t;
 
-_Static_assert(sizeof(ds_frame_t) == 17, "frame must be 17 bytes");
+_Static_assert(sizeof(ds_frame_t) == 12, "frame must be 12 bytes");
 
 void data_stream_init(void)
 {
@@ -38,11 +33,6 @@ bool data_stream_send(void)
         .yellow         = fret_scan_result(FRET_YELLOW),
         .blue           = fret_scan_result(FRET_BLUE),
         .orange         = fret_scan_result(FRET_ORANGE),
-        .green_pressed  = fret_is_pressed(FRET_GREEN),
-        .red_pressed    = fret_is_pressed(FRET_RED),
-        .yellow_pressed = fret_is_pressed(FRET_YELLOW),
-        .blue_pressed   = fret_is_pressed(FRET_BLUE),
-        .orange_pressed = fret_is_pressed(FRET_ORANGE),
         .end            = DS_END_BYTE,
     };
 
